1e2b7648744aab91d76b3d4dc0f830d337959c80
Isaac <your.email@example.com>
2025-01-23 01:40:37 +0300

    crud-code-along

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,17 +2,16 @@ import React from "react";
 
 function Filter({ category, onCategoryChange }) {
   return (
-    <div className="Filter">
-      <select
-        name="filter"
-        value={category}
-        onChange={(e) => onCategoryChange(e.target.value)}
-      >
-        <option value="All">Filter by category</option>
-        <option value="Produce">Produce</option>
-        <option value="Dairy">Dairy</option>
-        <option value="Dessert">Dessert</option>
-      </select>
+    <div>
+      <label>
+        Filter by Category:
+        <select value={category} onChange={(e) => onCategoryChange(e.target.value)}>
+          <option value="All">All</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Bakery">Bakery</option>
+        </select>
+      </label>
     </div>
   );
 }
